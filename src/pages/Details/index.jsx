747838d26b6623222a706d94ecde4dee673ed312
Lucas Andrade <lucas.goncalvessousa@aperam.com>
2023-08-30 11:29:09 -0300

    starting the frontend of rocketnotes project

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,15 @@
+import {Container} from './styles'
+import { Button } from '../../components/Button'
+
+export function Details() {
+    return (
+      <Container>
+        <h1>Hello World!</h1>
+        <span>lucas</span>
+
+        <Button title="Login" loading/>
+        <Button title="Cadastrar"/>
+        <Button title="Voltar"/>
+      </Container>
+    )
+}
