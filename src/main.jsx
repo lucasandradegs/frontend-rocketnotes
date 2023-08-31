6e7c2e8e2e59bd5f662d92418a4e7fe01a410acc
Lucas Andrade <lucas.goncalvessousa@aperam.com>
2023-08-31 14:44:58 -0300

    note description page finished

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details'
+import { Home } from './pages/Home'
 import { ThemeProvider } from 'styled-components'
 import theme from './pages/styles/theme'
 import GlobalStyles from './pages/styles/global'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>  
   </React.StrictMode>,
 )
